3641eb3ce02fe3209b0711879fc7ab491f991034
huxiaoming02 <huxiaoming02@58.com>
2021-03-18 20:50:32 +0800

    init

---
+++ examples/src/main/java/kafka/examples/MyProducer.java
@@ -0,0 +1,21 @@
+package kafka.examples;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class MyProducer {
+    public static void main(String[] args) {
+        Properties properties=new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer<Object, String> producer=new KafkaProducer<>(properties);
+        producer.send(new ProducerRecord<>("xiao", "12"));
+        producer.close();
+
+    }
+}
